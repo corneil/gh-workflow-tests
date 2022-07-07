c68aa880a3fead7e5daf3a7c7be77afda943aa20
Corneil du Plessis <pcorneil@vmware.com>
2022-07-07 13:20:37 +0200

    wait different times

--- job-script.sh
+++ job-script.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Starting Host = $ENV_HOST, Env = $ENV_NUMBER at $(date)"
-TIME=$(expr 30 * (${ENV_NUMBER} + 1))
+MUL=$(expr 1 + $ENV_NUMBER)
+TIME=$(expr 30 * $MUL)
 sleep ${TIME}s
 echo "Completed Host = $ENV_HOST, Env = $ENV_NUMBER at $(date)"
