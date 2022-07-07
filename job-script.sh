c3d2decbc3529163eaaa2e118305a3cf90ccbbfe
Corneil du Plessis <pcorneil@vmware.com>
2022-07-07 14:18:06 +0200

    improve concurrency settings

--- job-script.sh
+++ job-script.sh
@@ -4,4 +4,9 @@ MUL=$(expr 1 + $ENV_NUMBER)
 TIME=$(expr 30 \* $MUL)
 echo "Sleeping: $TIME seconds"
 sleep ${TIME}s
-echo "Completed Host = $ENV_HOST, Env = $ENV_NUMBER at $(date)"
+if [[ "$ENV_NUMBER" == "1" ]]; then
+    echo "Error Host = $ENV_HOST, Env = $ENV_NUMBER at $(date)"
+    exit $ENV_NUMBER
+else
+    echo "Completed Host = $ENV_HOST, Env = $ENV_NUMBER at $(date)"
+fi
